e17d285d652e465b914e9c39479936eefee3a6ac
widifaizakhmad <93120790+widifaizakhmad@users.noreply.github.com>
2023-12-15 21:43:58 +0700

    Update autocrop_image.py

--- application/controller/autocrop_image.py
+++ application/controller/autocrop_image.py
@@ -3,23 +3,20 @@ import os
 import cv2
 import mediapipe as mp
 import numpy as np
-
 def autocrop(user):
-
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-
-    x_ = []
-    y_ = []
     crop_dir = os.path.join(app.config['CROPED_IMAGE'], user)
     raw_dir = os.path.join(app.config['UPLOADED_IMAGE'], user)
     if not os.path.exists(crop_dir):
             os.makedirs(crop_dir)
             
     counter = 0
-    
+
     for img_path in os.listdir(os.path.join(raw_dir)):
+        x_ = []
+        y_ = []
         data_aux = []
         img= cv2.imread(os.path.join(raw_dir, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -38,22 +35,26 @@ def autocrop(user):
                 x2 =int(max(x_) * W) 
                 y2 =int(max(y_) * H) 
                 x1, y1, x2, y2 = max(0, x1), max(0, y1), min(W, x2), min(H, y2)
-                
                 if (x2 - x1) > (y2 - y1):
+                    # Menyesuaikan y2 untuk menjaga rasio persegi
                     y2 = y1 + (x2 - x1)
-                    y2 = int(0.75 * y2 ) 
-                    y1 = int((y1 - (0.25 * y2) ))     
-                
+                    # Menyesuaikan tinggi persegi
+                    selisih = y2-y1
+                    y2 = int(0.85 * y2)
+                    y1 = int(y2-selisih)
                 else:
+                    # Menyesuaikan x2 untuk menjaga rasio persegi
                     x2 = x1 + (y2 - y1)
-                    x2 = int(0.75 * x2)
-                    x1 = int((x1 - (0.25 * x2)))
-                
-                x1=int(x1-(x1*0.4))
-                x2=int(x2*1.4)
-                y1=int(y1-(y1*0.4))
-                y2=int(y2*1.4)
-             
+                    # Menyesuaikan lebar persegi
+                    selisih = x2-x1
+                    x2 = int(0.85 * x2)
+                    x1 = int(x2 - selisih)
+
+                x1 = int(x1)
+                x2 = int(x2)
+                y1 = int(y1)
+                y2 = int(y2)
+
                 hand_crop = img_rgb[y1:y2, x1:x2]
                 cv2.waitKey(25)
                 cv2.imwrite(os.path.join(crop_dir, '{}.jpg'.format(counter)),  cv2.cvtColor(hand_crop, cv2.COLOR_RGB2BGR))
